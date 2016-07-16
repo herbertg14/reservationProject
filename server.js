a2ffdb6f29b92ea1e086296a39a4a391c0413f83
herbertg14 <herbert.gutierrez@utexas.edu>
2016-07-16 11:09:34 -0500

    added port and redirect to home

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+var reservations = [];
+
+app.get("/", function (request, results){
+	res.sendFile(path.join(__dirname, "home.html"));
+})
+
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+})
