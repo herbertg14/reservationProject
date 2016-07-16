2018db2b945c9bd47de3ae1d7a92ceee44cc2094
Your Name <you@example.com>
2016-07-16 13:31:39 -0500

    capture user values for reservation

---
+++ saveReservation.js
@@ -0,0 +1,21 @@
+$('document').ready(function(){
+	console.log("JQuery Loaded");
+
+	$('#theButton').on('click',function(){
+		var name = $('#name').val().trim();
+		var phone = $('#phone').val().trim();
+		var email = $('#email').val().trim();
+		var unique_id = $('#unique_id').val().trim();
+
+		var theObject = {
+			name: name,
+			phone: phone,
+			email: email,
+			unique_id: unique_id
+		}
+		return theObject;
+	});
+
+
+
+});//end document.ready
